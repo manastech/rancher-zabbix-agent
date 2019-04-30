be7290fda258fd8ac18d450535b670f701a7d949
Juan Wajnerman <jwajnerman@manas.com.ar>
2019-04-30 11:10:19 -0300

    Renamed autoconfig environment variable to LISTENIP_FROM. Now it also sets the ZBX_PASSIVESERVERS with the whole CIDR block of the primary network device

--- rancher-entrypoint.sh
+++ rancher-entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-if [ "${LISTENIP_FROM}" != "" ]; then
-  case $LISTENIP_FROM in
+if [ "${AUTOCONFIG}" != "" ]; then
+  case $AUTOCONFIG in
     aws)
-      export ZBX_LISTENIP=$(curl -s 'http://169.254.169.254/latest/meta-data/local-ipv4')
+      MAC=$(curl -s http://169.254.169.254/latest/meta-data/mac)
+      export ZBX_PASSIVESERVERS=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC}/vpc-ipv4-cidr-block)
+      export ZBX_LISTENIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
       ;;
 
     *)
-      export ZBX_LISTENIP=$(ip -o -f inet addr show dev ${LISTENIP_FROM} | awk '{print $4}' | cut -d/ -f1)
+      export ZBX_PASSIVESERVERS=$(ip -o -f inet addr show dev ${AUTOCONFIG} | awk '{print $4}')
+      export ZBX_LISTENIP=$(ip -o -f inet addr show dev ${AUTOCONFIG} | awk '{print $4}' | cut -d/ -f1)
       ;;
   esac
 fi
